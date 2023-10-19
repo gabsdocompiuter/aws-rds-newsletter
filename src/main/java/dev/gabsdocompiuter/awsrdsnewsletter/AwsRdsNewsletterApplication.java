3d5f93c23aa5f0ac3416e7710435017b05b22d91
Gabriel Monteiro <gabsdocompiuter@gmail.com>
2023-10-19 12:24:43 -0300

    Create spring project

---
+++ src/main/java/dev/gabsdocompiuter/awsrdsnewsletter/AwsRdsNewsletterApplication.java
@@ -0,0 +1,13 @@
+package dev.gabsdocompiuter.awsrdsnewsletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsRdsNewsletterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsRdsNewsletterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gabsdocompiuter/awsrdsnewsletter/AwsRdsNewsletterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gabsdocompiuter.awsrdsnewsletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsRdsNewsletterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
